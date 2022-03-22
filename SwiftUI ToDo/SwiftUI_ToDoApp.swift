71637bb4e33763c477d9cc657dcbdf6bcce0faec
profoundA <81711479+profoundA@users.noreply.github.com>
2022-03-22 15:17:15 +0300

    Initial Commit

---
+++ SwiftUI ToDo/SwiftUI_ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToDoApp.swift
+//  SwiftUI ToDo
+//
+//  Created by Andrey Lobanov on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
